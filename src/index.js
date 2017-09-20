ae066b10b486c33031c8db9e40ea184a70463192
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-09-20 18:09:39 +0300

    Day1 - done 10 Rules under the belt total 6h work for 100 min video

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAzigPkoUft3TckvvF0JCvpfFIxkLgeRxQ'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+YTSearch({key: API_KEY, term: 'react redux meteor'}, function (data) {
+  console.log(data)
+})
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// desc:
+// 1. create a new component this will produce some html
+
+// const App = function () {
+// step 1.
+const AppZero = () => {
+  return <div>Hi there!</div>
+}
+// what is the purpose of JSX?!
+// make the writing simpler
+// http://babeljs.io/
+// const App = function () {
+//   return <div>Hi there!</div>
+// }
+//
+// const App2 = function () {
+//   return <ol>
+//     <li>1</li>
+//     <li>2</li>
+//     <li>3</li>
+//     </ol>
+// }
+
+const App = () => { // ES6
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )// return
+}
+
+// 2. take the components' generated html and put it on the page (in the DOM)
+
+ReactDOM.render(<AppZero />, document.querySelector('.container')) // this requires import React and ReactDOM
+ReactDOM.render(<App />, document.querySelector('.container')) // this requires import React and ReactDOM
+// ReactDOM.render(<App />, document.getElementById('container')) // this requires import React and ReactDOM
