51f84ca06fe179293819d24eb9d9977488654e0b
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-09-20 21:20:59 +0300

    using map to iterate elements of a matrix and using key Important map and key

---
+++ src/components/video_list.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import VideoListItem from './video_list_item'
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  })
+  return (
+    <ul className='col-md-4 list-group'>
+      {videoItems}
+    </ul>
+  )
+}
+
+export default VideoList
+
+// array.map(function(number) {return '<div>' + number + '</div>'})
+// >>> ["<div>1</div>", "<div>2</div>", "<div>3</div>"]
