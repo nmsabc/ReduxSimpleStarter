634b6e036270658de4a3030b9b2f9d3c9d908fa7
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-09-20 23:21:27 +0300

    we added the callback function and ensure that the selectedVideo is loaded onClick from the VideoListItem into the VideoList and into the App

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyAzigPkoUft3TckvvF0JCvpfFIxkLgeRxQ'
 
 class App extends Component { // ES6
   constructor (props) {
     super(props)
 
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
 
     YTSearch({key: API_KEY, term: 'react redux meteor'}, (videos) => {
-      this.setState({ videos })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     }) // YTSearch
   }// constructor
 
@@ -20,7 +27,10 @@ class App extends Component { // ES6
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
       </div>
     )// return
   } // render
