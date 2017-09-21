fde2c6f2e2a5ba63b368c0af1b7fb8c2d8852b26
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-09-21 22:34:34 +0300

    Section_3&4_Redux-Rule20-25.5 on Reducer and Container
    Initial app works fine just before the actions to be done

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function () {
+  return [
+    {title: 'this is how the cookie is done'},
+    {title: 'All the best of Javascript'},
+    {title: 'Who moved my cheese'},
+    {title: 'where are my socks'},
+    {title: 'go and code and do nice stuff'}
+  ]
+}
