074ce8cb323789e19f9546d00d8031bee3870fd8
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-10-17 08:01:45 +0300

    No-53-onInputChange-bind for passing this as a callback

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {term: ''}
+    this.onInputChange = this.onInputChange.bind(this)
+  } // constructor
+
+  onInputChange (event) {
+    console.log('printed: ', event.target.value)
+    this.setState({term: event.target.value})
+  } // onInputChange
+
   render () {
     return (
-      <form className='input-group'>
-        <input />
-        <span className='input-group-btn'>
-          <button type='submit' className='btn btn-secondary'>Submit</button>
-        </span>
-      </form>
+      <div className='jumbotron' >
+        <form className='input-group'>
+          <input
+            placeholder='Get a 5 days forecast in your fav cities'
+            className='form-control'
+            value={this.state.term}
+            onChange={this.onInputChange}
+          />
+          <span className='input-group-btn'>
+            <button type='submit' className='btn btn-primary'>Submit</button>
+          </span>
+        </form>
+      </div>
     ) // return
   }// render
 }// class
