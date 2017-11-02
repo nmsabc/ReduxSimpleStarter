da07699b563623ac71683b463774875067655b91
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-11-02 07:08:24 +0200

    No 66 - all charts well mapped and nice looking - next to google-maps

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,14 +6,14 @@ class WeatherList extends Component {
   renderWeather (cityData) {
     const name = cityData.city.name
     const temps = cityData.list.map(weather => weather.main.temp)
-    const presure = cityData.list.map(weather => weather.main.presure)
-    const humidity = cityData.list.map(weather => weather.main.humidity)
+    const presures = cityData.list.map(weather => weather.main.pressure)
+    const humidities = cityData.list.map(weather => weather.main.humidity)
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temps} color='orange' />
-        </td>
+        <td> <Chart data={temps} color='orange' units='C' /></td>
+        <td> <Chart data={presures} color='green' units='hPa' /></td>
+        <td> <Chart data={humidities} color='black' units='%' /></td>
       </tr>
     )
   }
@@ -23,9 +23,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
