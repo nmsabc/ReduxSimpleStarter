904fda2cb9e1e0c1e325684cd8c6a335770a5921
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-11-04 18:12:03 +0200

    No121 - simple router with Hi and By files including a HEADER - nice

--- src/index.js
+++ src/index.js
@@ -3,14 +3,31 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from './components/app'
 import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore)
 
+class Salut extends React.Component {
+  render () {
+    return <div> Hi there! </div>
+  }
+}
+class By extends React.Component {
+  render () {
+    return <div> La revedere! </div>
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        HEADER
+        <Route path='/hi' component={Salut} />
+        <Route path='/by' component={By} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'))
