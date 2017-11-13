fa29b47646fbf179e860d008d7136ce3ce8bb48e
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-11-13 13:08:02 +0200

    No 123: - routing for Html Index done

---
+++ src/components/post_index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class PostIndex extends Component {
+  render () {
+    console.log('*** Before', PostIndex)
+
+    return (
+      <div>
+        Head
+        <div>
+            Post Index
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PostIndex
--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import { BrowserRouter, Route } from 'react-router-dom'
-import App from './components/app'
+
 import reducers from './reducers'
+import PostIndex from './components/post_index'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore)
-
-class Salut extends React.Component {
-  render () {
-    return <div> Hi there! </div>
-  }
-}
-class By extends React.Component {
-  render () {
-    return <div> La revedere! </div>
-  }
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        HEADER
-        <Route path='/hi' component={Salut} />
-        <Route path='/by' component={By} />
+        <Route path='/' component={PostIndex} />
       </div>
     </BrowserRouter>
   </Provider>
